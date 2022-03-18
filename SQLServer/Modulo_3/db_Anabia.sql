fd466dda27bc680fc25d8423e50289a5f8798c06
AnnaBia <bia_espejo@hotmail.com>
2022-03-17 21:09:43 -0300

    customizando 'CHECK email' e adicionando exemplos de 'Desobedecendo as regras'

--- SQLServer/Modulo_3/db_Anabia.sql
+++ SQLServer/Modulo_3/db_Anabia.sql
@@ -42,7 +42,7 @@ CREATE TABLE  cliente(
 	CONSTRAINT valida_nome CHECK (nome LIKE '[A-Z]%'),
 	CONSTRAINT valida_cep CHECK (cep LIKE '[0-9][0-9][0-9][0-9][0-9][-][0-9][0-9][0-9]'),
 	CONSTRAINT valida_genero CHECK (genero LIKE 'F' OR genero LIKE 'M'),
-	CONSTRAINT valida_email CHECK (email LIKE '[a-z]%@[a-z]%'),
+	CONSTRAINT valida_email CHECK (email LIKE '[a-z0-9]%@[a-z0-9]%.[a-z0-9]%'),
 	CONSTRAINT valida_cpf CHECK (cpf LIKE '[0-9][0-9][0-9][.][0-9][0-9][0-9][.][0-9][0-9][0-9][-][0-9][0-9]')
 );
 GO
@@ -60,6 +60,8 @@ CREATE TABLE  telefone (
 	CONSTRAINT valida_telefone CHECK (telefone LIKE '[0-9][0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]')
 );
 GO
+
+-- a.	Obedecendo às regras
 -- insert table estado
 INSERT INTO estado (estado, sigla) VALUES ('Acre', 'AC');
 INSERT INTO estado (estado, sigla) VALUES ('Alagoas', 'AL');
@@ -77,15 +79,16 @@ INSERT INTO cidade (cidade, estado) VALUES ('Salvador', 5);
 INSERT INTO cidade (cidade, estado) VALUES ('Belo Horizonte', 6);
 
 -- insert table cliente
-INSERT INTO cliente (nome, endereco, numero, complem, bairro, cidade, estado, cep, genero, escolaridade, email, cpf) VALUES ('Letibo', 'Rua das Goiabas', 22, 'fazenda', 'Interior', 6, 6, '11111-999', 'F', 'Ensino Superior', 'etibo@email.com', '111.111.111-11');
-INSERT INTO cliente (nome, endereco, numero, complem, bairro, cidade, estado, cep, genero, escolaridade, email, cpf) VALUES ('Alejado', 'Rua das Goiabas', 22, 'fazenda', 'Interior', 6, 6, '11111-999', 'F', 'Ensino Superior', 'alejado@email.com', '111.111.111-11');
-INSERT INTO cliente (nome, endereco, numero, complem, bairro, cidade, estado, cep, genero, escolaridade, email, cpf) VALUES ('Gary', 'Rua das Goiabas', 22, 'fazenda', 'Interior', 6, 6, '11111-999', 'F', 'Ensino Superior', 'gary@email.com', '111.111.111-11');
-INSERT INTO cliente (nome, endereco, numero, complem, bairro, cidade, estado, cep, genero, escolaridade, email, cpf) VALUES ('Fanily', 'Rua das Goiabas', 22, 'fazenda', 'Interior', 6, 6, '11111-999', 'F', 'Ensino Superior', 'family@email.com', '111.111.111-11');
-INSERT INTO cliente (nome, endereco, numero, complem, bairro, cidade, estado, cep, genero, escolaridade, email, cpf) VALUES ('Jersel', 'Rua das Goiabas', 22, 'fazenda', 'Interior', 6, 6, '11111-999', 'F', 'Ensino Superior', 'jersel@email.com', '111.111.111-11');
+INSERT INTO cliente (nome, endereco, numero, complem, bairro, cidade, estado, cep, genero, escolaridade, email, cpf) VALUES ('Letibo', 'Rua das Goiabas', 22, 'fazenda', 'Interior', 6, 6, '11111-999', 'F', 'Ensino Superior', 'letibo@email.com', '111.111.111-11');
+INSERT INTO cliente (nome, endereco, numero, complem, bairro, cidade, estado, cep, genero, escolaridade, email, cpf) VALUES ('Alejado', 'Rua das Goiabas', 22, 'fazenda', 'Interior', 6, 6, '11111-999', 'M', 'Ensino Superior', 'alejado@email.com', '111.111.111-11');
+INSERT INTO cliente (nome, endereco, numero, complem, bairro, cidade, estado, cep, genero, escolaridade, email, cpf) VALUES ('Gary', 'Rua das Goiabas', 22, 'fazenda', 'Interior', 6, 6, '11111-999', 'M', 'Ensino Superior', 'gary@email.com', '111.111.111-11');
+INSERT INTO cliente (nome, endereco, numero, complem, bairro, cidade, estado, cep, genero, escolaridade, email, cpf) VALUES ('Family', 'Rua das Goiabas', 22, 'fazenda', 'Interior', 6, 6, '11111-999', 'F', 'Ensino Superior', 'family@email.com', '111.111.111-11');
+INSERT INTO cliente (nome, endereco, numero, complem, bairro, cidade, estado, cep, genero, escolaridade, email, cpf) VALUES ('Jersel', 'Rua das Goiabas', 22, 'fazenda', 'Interior', 6, 6, '11111-999', 'M', 'Ensino Superior', 'jersel@email.com', '111.111.111-11');
 INSERT INTO cliente (nome, endereco, numero, complem, bairro, cidade, estado, cep, genero, escolaridade, email, cpf) VALUES ('Angel', 'Rua das Goiabas', 22, 'fazenda', 'Interior', 6, 6, '11111-999', 'F', 'Ensino Superior', 'angel@email.com', '111.111.111-11');
-INSERT INTO cliente (nome, endereco, numero, complem, bairro, cidade, estado, cep, genero, escolaridade, email, cpf) VALUES ('Gaybriel', 'Rua das Goiabas', 22, 'fazenda', 'Interior', 6, 6, '11111-999', 'F', 'Ensino Superior', 'gaybriel@email.com', '111.111.111-11');
+INSERT INTO cliente (nome, endereco, numero, complem, bairro, cidade, estado, cep, genero, escolaridade, email, cpf) VALUES ('Gaybriel', 'Rua das Goiabas', 22, 'fazenda', 'Interior', 6, 6, '11111-999', 'M', 'Ensino Superior', 'gaybriel@email.com', '111.111.111-11');
 INSERT INTO cliente (nome, endereco, numero, complem, bairro, cidade, estado, cep, genero, escolaridade, email, cpf) VALUES ('Chéli', 'Rua das Goiabas', 22, 'fazenda', 'Interior', 6, 6, '11111-999', 'F', 'Ensino Superior', 'cheli@email.com', '111.111.111-11');
 
+-- b.	Utilizando os defaults
 -- insert table telefone
 INSERT INTO telefone (cliente, telefone) VALUES (1, '11111-2345');
 INSERT INTO telefone (cliente, telefone) VALUES (2, '11111-3456');
@@ -95,3 +98,9 @@ INSERT INTO telefone (cliente, telefone) VALUES (5, '11111-6789');
 INSERT INTO telefone (cliente, telefone) VALUES (6, '11111-7890');
 INSERT INTO telefone (cliente, telefone) VALUES (7, '11111-0123');
 INSERT INTO telefone (cliente, telefone) VALUES (8, '11111-1234');
+
+-- c.	Desobedecendo às regras
+INSERT INTO cliente (nome, endereco, numero, complem, bairro, cidade, estado, cep, genero, email, cpf) VALUES ('Nathalia', 'Rua das Gaivotas', 130, 'casa', 'Centro', 5, 5, '11111-999', 'F', 'nathalia@email', '111.111.111-11');
+INSERT INTO cliente (nome, endereco, numero, complem, bairro, cidade, estado, cep, genero, email, cpf) VALUES ('Pietro', 'Rua do Oriente', 550, 'ap', 'Cachoeirinha', 3, 3, '11111-999', 'E', 'pietro@email.com', '11111111111');
+INSERT INTO telefone (cliente, telefone) VALUES (6, '111115678');
+INSERT INTO telefone (cliente, telefone) VALUES (8, '111116789');
